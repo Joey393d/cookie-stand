0eb78c05de593e12db914a5c4291a882127dd59a
tektechnologies <info@tektechnologies.com>
2021-08-03 10:30:19 -0500

    add comments

--- js/app.js
+++ js/app.js
@@ -21,18 +21,22 @@ const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
 
 
 let storeOne = {
+  //set up of keys and values given to us.
   locationName: 'Seattle',
   minCustomerPerHour: 23,
   maxCustomerPerHour: 65,
   avgCookiesPerSale: 6.3,
+  //needed these to store information 
   customersEachHour: [],
   cookiesEachHour: [],
   totalDailyCookies: 0,
+  //calculate customers to store in an array
   calcCustomersEachHour: function(){
     for(let i = 0; i < hours.length; i++){
       this.customersEachHour.push(random(this.minCustomerPerHour, this.maxCustomerPerHour));
     }
   },
+  //calculate our cookies and call the customer each hour function
   calcCookiesEachHour: function(){
     this.calcCustomersEachHour();
     for (let i = 0; i < hours.length; i++){
@@ -41,19 +45,22 @@ let storeOne = {
       this.totalDailyCookies = this.totalDailyCookies + oneHour;
     }
   },
+  //we have the render function that calls the calc cookie function which calls the calc customers function
   render(){
     this.calcCookiesEachHour();
     const unorderedList = document.getElementById('seattle');
     for(let i = 0; i < hours.length; i++){
+      //here we get cookies each hour
       let listItem = document.createElement('li');
       listItem.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
+      //they go to the page
       unorderedList.appendChild(listItem);
     }//closes for loop
+    //lastly we get total cookies, base on calcs happening above.
     let listItem = document.createElement('li');
     listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
     unorderedList.appendChild(listItem);
   }
-
 };//closes object literal storeOne
 
 
@@ -62,7 +69,7 @@ let storeOne = {
 
 
 
-
+//runs our random number in our objects
 function random(minCust, maxCust){
   return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
 }
